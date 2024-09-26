009417645806c8c2cd8730db2089faafe355226d
lachl <lachlan.webb@my.jcu.edu.au>
2024-09-26 14:10:45 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,21 @@
-password_limit = 5
+def main():
+    password_limit = 5
 
-password_guess = input('Enter a password: ')
+    password_guess = get_password(password_limit)
+    convert_password_to_asterisk(password_guess)
 
-while len(password_guess) < password_limit:
-    print(f"Password need to be more than {password_limit} characters long")
+
+def convert_password_to_asterisk(password_guess):
+    for i in range(len(password_guess)):
+        print('*', end="")
+
+
+def get_password(password_limit):
     password_guess = input('Enter a password: ')
-for i in range(len(password_guess)):
-    print('*', end="")
+    while len(password_guess) < password_limit:
+        print(f"Password need to be more than {password_limit} characters long")
+        password_guess = input('Enter a password: ')
+    return password_guess
+
+
+main()
