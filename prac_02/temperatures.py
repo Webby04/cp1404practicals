d0e4e9f2ddd1817a0a90746e5da811de291c6e2c
lachl <lachlan.webb@my.jcu.edu.au>
2024-09-28 15:19:25 +1000

    Add a function for each conversion rate

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -13,9 +13,14 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius_to_fahrenheit()
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
-            fahrenheit_to_celsius()
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+
         else:
             print("Invalid option")
         print(MENU)
@@ -23,16 +28,11 @@ def main():
     print("Thank you.")
 
 
-def celsius_to_fahrenheit():
-    celsius = float(input("Celsius: "))
-    fahrenheit = celsius * 9.0 / 5 + 32
-    print(f"Result: {fahrenheit:.2f} F")
+def celsius_to_fahrenheit(celsius):
+     return celsius * 9.0 / 5 + 32
 
 
-def fahrenheit_to_celsius():
-    fahrenheit = float(input("Fahrenheit: "))
-    celsius = 5 / 9 * (fahrenheit - 32)
-    print(f"Result: {celsius:.2f} C")
-
+def fahrenheit_to_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
 
 main()
