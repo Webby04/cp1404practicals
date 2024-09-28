c5d469b3b6959e8fda27fa915f61dafd63b9430c
lachl <lachlan.webb@my.jcu.edu.au>
2024-09-28 15:27:15 +1000

    Add prac_01 and refactor

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -12,5 +12,5 @@ for i in range(1, number_of_items + 1):
     price = float(input("Price of item: "))
     total_price += price
 if total_price > 100:
-    total_price += total_price * 0.1
+    total_price -= total_price * 0.1
 print(f"Total price for {number_of_items} items is {total_price:.2f}")
