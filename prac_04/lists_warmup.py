c8f063227ecb1b3f306c14d273fa415598ce7d9d
lachl <lachlan.webb@my.jcu.edu.au>
2024-10-09 13:25:22 +1000

    Add changes to the list and print them.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,10 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Change first element to "ten"
+numbers[0] = "ten"
+# Change last element to 1
+numbers[-1] = 1
+# Print all numbers except first two
+print(numbers[2:])
+# Check whether 9 is in numbers
+print(9 in numbers)
