55ae9d8376ab7dff2bc997555cb9f2dd7f5ab86f
lachl <lachlan.webb@my.jcu.edu.au>
2024-10-10 14:45:27 +1000

    Check if username is in list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,3 +15,11 @@ print(f"The smallest number is {smallest_number}")
 print(f"The largest number is {largest_number}")
 print(f"The average of the numbers is {average_number}")
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("What is your username? ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
