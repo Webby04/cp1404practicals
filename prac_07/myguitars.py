b2425ef85497413bbd4958af605a0124178e9b5f
lachl <lachlan.webb@my.jcu.edu.au>
2024-11-09 12:19:32 +1000

    display guitars in the list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,10 +5,14 @@ FILENAME = "guitars.csv"
 def main():
     """Read guitars from file and display them."""
     guitars = []
-    with open(FILENAME) as csvfile:
+    with open(FILENAME, 'r') as csvfile:
         for line in csvfile:
             name, year, cost = line.strip().split(',')
             guitar = Guitar(name, int(year), float(cost))
             guitars.append(guitar)
+
+    for guitar in guitars:
+        print(guitar)
+
 
 main()
