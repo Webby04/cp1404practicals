10829e768a3441762b11ba2f80a428e25c070c7a
lachl <lachlan.webb@my.jcu.edu.au>
2024-11-09 12:22:44 +1000

    put code in functions and add docstrings

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,13 +4,23 @@ FILENAME = "guitars.csv"
 
 def main():
     """Read guitars from file and display them."""
+    guitars = load_guitars()
+    display_guitars(guitars)
+
+
+def load_guitars():
+    """Load guitars from CSV file and return list of Guitar objects."""
     guitars = []
     with open(FILENAME, 'r') as csvfile:
         for line in csvfile:
             name, year, cost = line.strip().split(',')
             guitar = Guitar(name, int(year), float(cost))
             guitars.append(guitar)
+    return guitars
 
+
+def display_guitars(guitars):
+    """Display all the guitars in the list."""
     for guitar in guitars:
         print(guitar)
 
