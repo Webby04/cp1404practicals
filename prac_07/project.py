eb3641b0a2a2975758cca041601b3a4fa9387a4d
lachl <lachlan.webb@my.jcu.edu.au>
2024-11-10 16:43:14 +1000

    add init and str methods for class

---
+++ prac_07/project.py
@@ -0,0 +1,18 @@
+"""
+Project
+Estimate: 90 minutes
+Actual:   23 minutes
+"""
+import datetime
+
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: $"
+                f"{self.cost_estimate}, completion: {self.completion_percentage}%")
