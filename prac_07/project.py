571f3566cd44de07e97c3626dd6ba52f4061fbb1
lachl <lachlan.webb@my.jcu.edu.au>
2024-11-10 22:21:23 +1000

    add method to sort by date

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,7 +3,6 @@ Project
 Estimate: 90 minutes
 Actual:   23 minutes
 """
-import datetime
 
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
@@ -15,11 +14,23 @@ class Project:
 
     def __str__(self):
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: $"
-                f"{self.cost_estimate}, completion: {self.completion_percentage}%")
+                f"{self.cost_estimate:,.2f}, completion: {self.completion_percentage}%")
 
     def __lt__(self, other):
+        """Compare projects by priority (ascending)."""
         return self.priority < other.priority
+
+    def __gt__(self, other):
+        """Compare two projects by start date (ascending)"""
+        return self.start_date > other.start_date
 
     def is_complete(self):
         """Return True if projects is 100% complete."""
         return self.completion_percentage == 100
+
+    def update_percentage(self, new_percentage):
+        self.completion_percentage = new_percentage
+
+    def update_priority(self, new_priority):
+        self.priority = new_priority
+
